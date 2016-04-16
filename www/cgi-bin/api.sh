bf2d1e6d37a714b785126e813a7b00e60de07360
bwelker <bwelker@gmail.com>
2016-04-16 21:59:57 +0200

    Add Value for output to config

--- www/cgi-bin/api.sh
+++ www/cgi-bin/api.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+CONFIGCFG=/usr/local/etc/config.cfg
 saveIFS=$IFS
 IFS='&'
 eval $QUERY_STRING
@@ -12,10 +12,12 @@ case $q in
                         "hdmi" )
                                 echo "switching to HDMI"
                                 amixer cset numid=3 2 >> /dev/null
+                                sudo sed -i "s/\(AUDIO=\).*/\1\"HDMI\"/" $CONFIGCFG
                                 ;;
                         "analog" )
                                 echo "switching to 3.5 jack"
                                 amixer cset numid=3 1 >> /dev/null
+                                sudo sed -i "s/\(AUDIO=\).*/\1\"Analog\"/" $CONFIGCFG
                                 ;;
                 esac
                 ;;
@@ -31,6 +33,9 @@ case $q in
                                 ;;
                         "hostname" )
                                 echo "setting Hostname to "
+                                # This will set the hostname next time.
+                                sudo sed -i "s/\( host=\)[a-zA-Z0-9\-]*//g" $CMDLINETXT
+                                sudo sed -i "1 s/$/ host=$HOST/" $CMDLINETXT
                                 ;;
                 esac
                 ;;
